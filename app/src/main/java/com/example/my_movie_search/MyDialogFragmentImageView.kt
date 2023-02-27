4b17742c016846bba3d51db9e467cf6a07110699
TolsVA <himtol@yandex.ru>
2023-02-27 15:10:17 +0700

    add homework 1

---
+++ app/src/main/java/com/example/my_movie_search/MyDialogFragmentImageView.kt
@@ -0,0 +1,54 @@
+package com.example.my_movie_search
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.res.TypedArray
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+
+class MyDialogFragmentImageView : DialogFragment() {
+    private var pager2: ViewPager2? = null
+    var icons: TypedArray? = null
+    var resourceId = 0
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val customView = layoutInflater.inflate(R.layout.icon_name_group, null)
+        val editText = customView.findViewById<EditText>(R.id.folder_name)
+        icons = resources.obtainTypedArray(R.array.avatar)
+        pager2 = customView.findViewById(R.id.pager_photo)
+        pager2!!.adapter = MyAdapterIcon(this, icons!!)
+        pager2!!.setPageTransformer(ZoomOutPageTransformer())
+        savePosition()
+        return AlertDialog.Builder(requireContext())
+            .setView(customView)
+            .setPositiveButton(resources.getString(R.string.create_contact)) { _: DialogInterface?, _: Int ->
+                val user = User(resourceId, editText.text.toString())
+                val activity: Activity = requireActivity()
+                (activity as MainActivity).create(user)
+            }
+            .setNeutralButton(resources.getString(R.string.cancel), null)
+            .create()
+    }
+
+    private fun savePosition() {
+        pager2!!.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            @SuppressLint("UseCompatLoadingForDrawables")
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                resourceId = icons!!.getResourceId(position, 0)
+            }
+        })
+    }
+
+    companion object {
+        const val TAG = "MyDialogFragmentImageView"
+        fun newInstance(): MyDialogFragmentImageView {
+            return MyDialogFragmentImageView()
+        }
+    }
+}
